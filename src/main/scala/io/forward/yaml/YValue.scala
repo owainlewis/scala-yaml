aa23cd7b62091f41220c508bbc88ed5687201167
Owain Lewis <owain@owainlewis.com>
2015-12-29 10:21:53 +0000

    Update YValue.scala

--- src/main/scala/io/forward/yaml/YValue.scala
+++ src/main/scala/io/forward/yaml/YValue.scala
@@ -7,15 +7,14 @@ abstract class YValue {
   /** Arrow traverse */
   def >>>(v: String): Option[YValue]
 
-  def isNull: Boolean =
-    this == YNull
+  def isNull: Boolean = this == YNull
 
   def name: String =
     this match {
       case YNull       => "Null"
       case YBoolean(_) => "Boolean"
       case YNumber(_)  => "Number"
-      case YSet(_) => "Set"
+      case YSet(_)     => "Set"
       case YString(_)  => "String"
       case YObj(_)     => "Object"
       case YSeq(_)     => "Seq"
@@ -25,39 +24,30 @@ abstract class YValue {
 }
 
 case class YObj(value: Map[String, YValue]) extends YValue {
-
   def asSnakeYAML = null // TODO
-
   def >>>(v: String): Option[YValue] = value.get(v)
 }
 
 case class YNumber[A](value: A)(val num: Numeric[A]) extends YValue {
-
   def asSnakeYAML = null // TODO
-
   def >>>(v: String) = None
 }
 
 case class YString(value: String) extends YValue {
   def asSnakeYAML = null
-
   def >>>(v: String) = None
 }
 
 sealed abstract class YBoolean extends YValue {
   def value: Boolean
-
   def asSnakeYAML = null
-
   def >>>(v: String) = None
 }
 
 object YBoolean extends YValue {
   def apply(x: Boolean): YBoolean = if (x) YTrue else YFalse
   def unapply(x: YBoolean): Option[Boolean] = Some(x.value)
-
   def asSnakeYAML = this
-
   def >>>(v: String) = None
 }
 
@@ -66,13 +56,9 @@ case object YTrue  extends YBoolean  { def value = true  }
 case object YFalse extends YBoolean  { def value = false }
 
 case class YSeq(elements: Vector[YValue]) extends YValue {
-
   def map[B](f: YValue => B) = this.elements map f
-
   def isEmpty: Boolean = this.elements.isEmpty
-
   def asSnakeYAML = seqAsJavaList(elements.map(_.asSnakeYAML))
-
   def >>>(v: String) = None
 }
 
@@ -81,13 +67,9 @@ object YSeq{
 }
 
 case class YSet(elements: Set[YValue]) extends YValue {
-
   def map[B](f: YValue => B) = this.elements map f
-
   def isEmpty: Boolean = this.elements.isEmpty
-
   def asSnakeYAML = setAsJavaSet(elements.map(_.asSnakeYAML))
-
   def >>>(v: String) = None
 }
 
@@ -98,6 +80,5 @@ object YSet {
 
 case object YNull extends YValue {
   def asSnakeYAML = null
-
   def >>>(v: String) = None
 }
